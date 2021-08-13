fbd5b80e0c132346ea5a719ea1d1af37b633a92b
Amir Samary <amirsamary@gmail.com>
2021-08-13 13:10:22 -0400

    Initial commit

--- 3-consume-test-events.sh
+++ 3-consume-test-events.sh
@@ -6,3 +6,4 @@ source ./utils.sh
 trace "Connecting from local machine where kafka broker is running as docker container. At the end, CTRL+C to stop pulling."
 docker-compose exec broker  \
 kafka-console-consumer --bootstrap-server localhost:29092 --topic foo --from-beginning
+
